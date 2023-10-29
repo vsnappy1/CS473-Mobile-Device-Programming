10005ae6e5b58fa597de8dda24e40e1f85173935
Vishal Kumar <vsnappy1@gmail.com>
2023-10-29 16:04:07 -0400

    Completed assignment 2

--- app/src/main/java/com/kumar/mdp/MainActivity.kt
+++ app/src/main/java/com/kumar/mdp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.kumar.mdp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kumar.mdp.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    val foodItems = mutableListOf("Hamburger", "Pizza", "Mexican", "American", "Chinese")
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonAddFood.setOnClickListener {
+            val foodItem = binding.editTextTextAddNewFood.text.toString().trim()
+            if(foodItem.isNotEmpty()){
+                foodItems.add(foodItem)
+                binding.editTextTextAddNewFood.text.clear()
+            }
+        }
+
+        binding.buttonDecideFood.setOnClickListener {
+            val index = Random.nextInt(0,foodItems.size)
+            binding.textViewFoodChoice.text = foodItems[index]
+        }
     }
 }
