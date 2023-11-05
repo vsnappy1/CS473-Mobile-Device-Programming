2fb3c78a464d8702017b960a182db10b6aa0c4fa
Vishal Kumar <vsnappy1@gmail.com>
2023-11-05 14:36:03 -0500

    Problem 2 solved.

---
+++ app/src/main/java/com/kumar/mdp/WalmartLoginPage.kt
@@ -0,0 +1,11 @@
+package com.kumar.mdp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WalmartLoginPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_walmart_login_page)
+    }
+}
