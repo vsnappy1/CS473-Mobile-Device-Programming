433c141fa1d9dd82ff0bfcceed6468e7b91340e7
Vishal Kumar <vsnappy1@gmail.com>
2023-11-05 13:15:38 -0500

    Enabled viewBinding

--- app/src/main/java/com/kumar/mdp/MainActivity.kt
+++ app/src/main/java/com/kumar/mdp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.kumar.mdp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kumar.mdp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
