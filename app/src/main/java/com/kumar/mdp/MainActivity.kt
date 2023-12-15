a213ae4debeb356156c62b6b4183b14e8a8a4477
Vishal Kumar <vsnappy1@gmail.com>
2023-12-14 20:58:44 -0500

    Setup for Jetpack compose done

--- app/src/main/java/com/kumar/mdp/MainActivity.kt
+++ app/src/main/java/com/kumar/mdp/MainActivity.kt
@@ -2,14 +2,28 @@ package com.kumar.mdp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.kumar.mdp.databinding.ActivityMainBinding
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContent {
+            HelloWorld()
+        }
     }
+}
+
+@Composable
+fun HelloWorld(){
+    Text(text = "Hello World")
+}
+
+@Preview
+@Composable
+fun PreviewHelloWorld(){
+    HelloWorld()
 }
